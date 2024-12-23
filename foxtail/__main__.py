a836720a205d420d7537e128eb073d6c80bb46c8
BlakeJC94 <blakejamescook@gmail.com>
2024-12-23 15:18:28 +1100

    rfc: before/after args

--- foxtail/__main__.py
+++ foxtail/__main__.py
@@ -11,13 +11,11 @@ import argparse
 import os
 import shutil
 import sqlite3
-from datetime import datetime, timedelta
 import sys
 from dataclasses import dataclass
+from datetime import datetime, timedelta, timezone
 from itertools import groupby
 from pathlib import Path
-from time import time
-from typing import Dict, List, Tuple
 from warnings import warn
 
 VERSION = "1.0.1"
@@ -42,10 +40,40 @@ class Result:
 
 def parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("firefox_dir", nargs="?", default="~/.mozilla/firefox")
-    parser.add_argument("--hours", type=int, default=0)
-    parser.add_argument("--days", type=int, default=7)
-    parser.add_argument("-v", "--version", action="store_true", default=False)
+    cur_time = datetime.now().astimezone()
+
+    parser.add_argument(
+        "firefox_dir",
+        nargs="?",
+        default="~/.mozilla/firefox",
+    )
+
+    parser.add_argument(
+        "--after",
+        type=str,
+        default=(cur_time - timedelta(days=7)).isoformat(),
+    )
+    parser.add_argument(
+        "--before",
+        type=str,
+        default=cur_time.isoformat(),
+    )
+
+    parser.add_argument(
+        "-f",
+        "--format",
+        type=str,
+        default="markdown",
+        choices=["markdown", "table"],
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        default=False,
+    )
+    # parser.add_argument("-i", "--interactive", action="store_true", default=False)
     return parser.parse_args()
 
 
@@ -55,17 +83,28 @@ def foxtail() -> list[str]:
         print(VERSION)
         return
 
-    delta = timedelta(days=args.days, hours=args.hours).total_seconds()
+    if args.before < args.after:
+        raise ValueError()
+
+    after = datetime.fromisoformat(args.after)
+    before = datetime.fromisoformat(args.before)
 
     database = get_database(args.firefox_dir)
 
-    lines = format_results(results)
-    print("\n".join(lines))
     database_cp = CACHE_PATH / "places.sqlite"
     database_cp.parent.mkdir(exist_okay=True, parents=True)
     shutil.copy(database, database_cp)
 
     results = query_database(database_cp, after=after, before=before)
+
+    # TODO interactive mode
+
+    if args.format == "markdown":
+        lines = format_results_markdown(results)
+    else:
+        lines = format_results_table(results)
+
+    return lines
 
 
 def get_database(firefox_dir: Path | str) -> Path:
@@ -82,7 +121,6 @@ def get_database(firefox_dir: Path | str) -> Path:
     return database
 
 
-    cur_time = time()
 def query_database(database: Path, after: datetime, before: datetime) -> list[Result]:
     with sqlite3.connect(database) as con:
         cursor = con.cursor()
@@ -91,32 +129,38 @@ def query_database(database: Path, after: datetime, before: datetime) -> list[Re
         SELECT p.url, b.title, b.dateAdded
         FROM moz_places p INNER JOIN moz_bookmarks b
         ON p.id = b.fk
-        WHERE b.dateAdded > {(cur_time - delta) * 1e6};
+        WHERE b.dateAdded > {after.timestamp() * 1e6}
+        AND b.dateAdded < {before.timestamp() * 1e6};
         """
 
         return [Result(*row) for row in cursor.execute(query).fetchall()]
 
 
-    grouped_results: Dict[str, List[Tuple[str, str, int]]] = {
-        k: list(v)
 def format_results_table(results: list[Result]) -> list[str]:
+    return [
+        "\t".join([result.get_date(), result.title, result.url])
+        for result in sorted(results, key=lambda x: x.time)
+    ]
+
+
+def format_results_markdown(results: list[Result]) -> list[str]:
+    grouped_results = {
+        k: sorted(v, key=lambda x: x.time)
         for k, v in groupby(
-            results, key=lambda x: datetime.fromtimestamp(int(x[2] / 1e6)).date()
+            results,
+            key=lambda x: x.get_date(),
         )
     }
 
     lines = []
-    lines.append("# Bookmarks from the last week")
-    lines.append("")
-
     for date in sorted(grouped_results.keys()):
         lines.append(f"## {date}")
         lines.append("")
 
-        date_results = sorted(grouped_results[date], key=lambda x: x[2])
-        for url, title_raw, _ in date_results:
-            title = title_raw.replace("]", "\]").replace("[", "\[")
-            lines.append(f"[{title}]({url})")
+        date_results = sorted(grouped_results[date], key=lambda x: x.time)
+        for result in date_results:
+            title_escaped = result.title.replace("]", "\]").replace("[", "\[")
+            lines.append(f"[{title_escaped}]({result.url})")
             lines.append("")
 
     return lines
